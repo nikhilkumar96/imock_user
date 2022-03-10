dbb170117b2671aad8cdfbeb0c08dc72873d9dd7
Nikhil Kumar <nikhilkumar9.kumar@gmail.com>
2022-03-11 00:13:02 +0530

    Register User Whole Post call flow complete

--- constant.py
+++ constant.py
@@ -1,3 +1,6 @@
 SERVICENAME = 'user'
 DB_CONN_URI = "mongodb://localhost:27017/imock?retryWrites=true&w=majority"
+DB_BASIC_CONN_URI = "mongodb://localhost:27017/"
+DB_NAME = "imock"
+USER_COLLECTION = "users"
 
